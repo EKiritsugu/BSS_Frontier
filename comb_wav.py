9d2ba0c7dbe160db18e58e5fc1b580395d22afe6
AR_Git <780355708@qq.com>
2022-02-11 19:27:14 +0800

    new record

--- comb_wav.py
+++ comb_wav.py
@@ -5,11 +5,12 @@ from scipy.io import wavfile
 # audio1,sr = sf.read('und/F1.wav')
 # audio2,sr = sf.read('und/F2.wav')
 
-audio = np.array([])
+
 gender = ['F','M']
 
 for i in gender:
-    for j in range(6):
+    audio = np.array([])
+    for j in range(9):
         k = j+1
         filename = 'und/'+i+str(k)+'.wav'
         audio_tmp , sr = sf.read(filename)
